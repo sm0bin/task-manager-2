0a6193a640ca2f25c2e6601f6482c2a7eaab8da8
GhostMobin <s1810677110@ru.ac.bd>
2024-01-02 19:43:11 +0600

    drag and drop success

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,71 +4,18 @@ import useAuth from "../hooks/useAuth";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import { toast } from "react-hot-toast";
 import useLoadDataSecure from "../hooks/useLoadDataSecure";
-import { useRef, useState } from "react";
 import Swal from "sweetalert2";
 import UpdateForm from "../components/dashboard/UpdateForm";
 import { useDrag, useDrop } from "react-dnd";
+import { useState } from "react";
 // import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const Dashboard = () => {
-    const ref = useRef(null);
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
     const [tasks, isPendingTasks, refetchTasks] = useLoadDataSecure(`/tasks/${user.email}`, "tasks");
-    const [updateTask, setUpdateTask] = useState({});
+
     const states = ["To Do", "Ongoing", "Completed"];
-
-    // const [, drop] = useDrop({
-    //     // Accept will make sure only these element type can be droppable on this element
-    //     accept: type,
-    //     hover(item) {
-    //         //   ...
-    //     }
-    // });
-
-
-    // const [{ isDragging }, drag] = useDrag(() => ({
-    //     // what type of item this to determine if a drop target accepts it
-    //     type: type,
-    //     // data of the item to be available to the drop methods
-    //     item: { id: image.id, index },
-    //     // method to collect additional data for drop handling like whether is currently being dragged
-    //     collect: (monitor) => {
-    //         return {
-    //             isDragging: monitor.isDragging(),
-    //         };
-    //     },
-    // }));
-
-    // if (isPendingTasks) {
-    //     return <div className="fixed inset-0 w-full h-screen flex items-center justify-center">
-    //         <span className="loading loading-spinner loading-lg"></span>
-    //     </div>
-    // }
-    // console.log(task);
-
-    // if (isPendingTasks) return <div>Loading...</div>
-    const addItemToSection = (id) => {
-        console.log('drop', id);
-    }
-
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: 'task',
-        drop: (item) => addItemToSection(item.id),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver()
-        })
-    }))
-
-    const [{ isDragging }, drag] = useDrag(() => ({
-        type: 'task',
-        item: { id: tasks?._id },
-        collect: (monitor) => ({
-            isDragging: !!monitor.isDragging()
-        })
-    }))
-
-
 
     const {
         register,
@@ -101,43 +48,6 @@ const Dashboard = () => {
 
     }
 
-
-
-
-    const deleteTask = (_id) => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                axiosSecure.delete(`/tasks/${_id}`)
-                    .then(res => {
-                        console.log(res.data);
-                        toast.success('Task Deleted!');
-                        refetchTasks();
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    })
-            }
-        });
-
-    }
-
-    const handleUpdateTask = (task) => {
-        console.log(task);
-        setUpdateTask(task);
-        console.log(updateTask);
-        if (updateTask) {
-            document.getElementById('updateTaskModal').showModal();
-        }
-    }
-
     // email
     // title
     // details
@@ -159,53 +69,12 @@ const Dashboard = () => {
                     Add Task</button>
             </div>
 
-            {/* Tasks */}
-            <section ref={drop} className="grid  grid-cols-1 lg:grid-cols-3 gap-4 my-4">
+            {/* Tasks Section */}
+            <section className="grid  grid-cols-1 lg:grid-cols-3 gap-4 my-4">
                 {
                     states.map(state => {
                         return (
-                            <div key={state} className="bg-base-300 rounded-lg">
-                                <div className="bg-neutral text-center p-4 rounded-t-lg text-white">
-                                    <h3 className="font-bold text-xl">{state}</h3>
-                                </div>
-
-                                <div className="p-4 space-y-4">
-                                    {
-                                        tasks?.map(task => {
-                                            if (task.state === state) {
-                                                return (
-                                                    <div ref={drag} key={task?._id} className={` ${isDragging ? '-rotate-2 cursor-grabbing' : ''} cursor-grab bg-gray-50 rounded-md overflow-hidden group`}>
-
-                                                        <div className="bg-neutral px-4 py-3 flex items-center justify-between gap-6">
-                                                            <h4 className="font-semibold text-gray-50 text-lg">{task?.title}</h4>
-
-                                                            <div className="flex">
-                                                                <button onClick={() => deleteTask(task._id)} className="btn btn-ghost btn-circle text-transparent group-hover:text-rose-400">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                                                        <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
-                                                                    </svg>
-
-                                                                </button>
-                                                                <button onClick={() => handleUpdateTask(task)} className="btn btn-ghost btn-circle text-transparent group-hover:text-green-400">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                                                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
-                                                                        <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
-                                                                    </svg>
-                                                                </button>
-                                                            </div>
-                                                        </div>
-
-                                                        <div className="p-4">
-                                                            {task?.details}
-                                                        </div>
-                                                    </div>
-                                                )
-                                            }
-                                        })
-                                    }
-                                </div>
-
-                            </div>
+                            <Section key={state} state={state} tasks={tasks} refetchTasks={refetchTasks} />
                         )
                     })
                 }
@@ -273,14 +142,137 @@ const Dashboard = () => {
                     <form method="dialog">
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
-
-                    <UpdateForm updateTask={updateTask} refetchTasks={refetchTasks} />
-
+                    <UpdateForm refetchTasks={refetchTasks} />
                 </div>
             </dialog>
 
         </div>
     );
 };
+
+
+
+
+const Section = ({ state, tasks, refetchTasks }) => {
+    const axiosSecure = useAxiosSecure();
+
+    const addItemToSection = (id) => {
+        console.log('drop', id);
+
+        axiosSecure.put(`/tasks/${id}`, { state })
+            .then(res => {
+                console.log(res.data);
+                toast.success('Task Updated!');
+                refetchTasks();
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: 'task',
+        drop: (item) => addItemToSection(item.id),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+        })
+    }))
+
+
+    return (
+        <div className="flex flex-col">
+            <div className="bg-neutral text-center p-4 rounded-t-lg text-white">
+                <h3 className="font-bold text-xl">{state}</h3>
+            </div>
+
+            <div ref={drop} className={`p-4 space-y-4 grow bg-base-300 rounded-b-lg ${isOver ? 'border border-dashed border-slate-700' : ''}`}>
+                {
+                    tasks?.map(task => {
+                        if (task.state === state) {
+                            return (
+                                <Task key={task._id} task={task} refetchTasks={refetchTasks} />
+                            )
+                        }
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+
+const Task = ({ task, refetchTasks }) => {
+    const axiosSecure = useAxiosSecure();
+    const [updateTask, setUpdateTask] = useState({});
+
+    const deleteTask = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/tasks/${_id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        toast.success('Task Deleted!');
+                        refetchTasks();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+            }
+        });
+
+    }
+
+    const handleUpdateTask = (task) => {
+        console.log(task);
+        setUpdateTask(task);
+        console.log(updateTask);
+        if (updateTask) {
+            document.getElementById('updateTaskModal').showModal();
+        }
+    }
+
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: 'task',
+        item: { id: task?._id },
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+
+    return (
+        <div ref={drag} key={task?._id} className={` ${isDragging ? '-rotate-2 cursor-grabbing' : ''} cursor-grab bg-gray-50 rounded-md overflow-hidden group`}>
+
+            <div className="bg-neutral px-4 py-3 flex items-center justify-between gap-6">
+                <h4 className="font-semibold text-gray-50 text-lg">{task?.title}</h4>
+
+                <div className="flex">
+                    <button onClick={() => deleteTask(task._id)} className="btn btn-ghost btn-circle text-transparent group-hover:text-rose-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
+                        </svg>
+
+                    </button>
+                    <button onClick={() => handleUpdateTask(task)} className="btn btn-ghost btn-circle text-transparent group-hover:text-green-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                            <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+            <div className="p-4">
+                {task?.details}
+            </div>
+        </div>
+    )
+}
 
 export default Dashboard;
